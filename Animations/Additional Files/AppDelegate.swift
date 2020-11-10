31805821fa783f006c0cde262f9c20cc94cf0e99
Evgeny Novgorodov <blissfulman01@gmail.com>
2020-11-10 21:01:11 +0500

    Empty project

---
+++ Animations/Additional Files/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Animations
+//
+//  Created by User on 10.11.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
